f69dca5e58b76ad02a87d498b45850fde9a264c6
Gonzalo Arana <gonzalo.arana@gmail.com>
2024-07-09 06:45:11 -0300

    Initial commit.

---
+++ src/o1shm.c
@@ -0,0 +1,83 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Gonzalo Arana
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/mman.h>
+
+#include "o1shm.h"
+
+O1_SHM_STATIC void* shm_alloc(
+    int* fd,
+    const char* filename,
+    off_t size
+) {
+    void* location = NULL;
+    int fd_ = shm_open(
+        filename,
+        O_CREAT | O_RDWR,
+        0600
+    );
+
+    if (fd_ < 0)
+        return SHM_ALLOC_OPEN_ERROR;
+
+    if (ftruncate(fd_, size) < 0) {
+        close(fd_);
+        return SHM_ALLOC_RESIZE_ERROR;
+    }
+
+    location = mmap(
+        NULL,
+        size,
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED,
+        fd_,
+        0
+    );
+
+    if (!location) {
+        close(fd_);
+        return SHM_ALLOC_MAP_ERROR;
+    }
+
+    *fd = fd_;
+    return location;
+}
+
+O1_SHM_STATIC int shm_unmap(void* p, off_t size) {
+    return munmap(p, size);
+}
